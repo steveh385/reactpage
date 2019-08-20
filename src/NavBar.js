e28708fb862932cd1c45f1b60ec70c0d1efe0d52
steveh385 <steveh385@hotmail.com>
2019-08-20 15:47:38 +0400

    reworked site

--- src/NavBar.js
+++ src/NavBar.js
@@ -1,21 +1,38 @@
 import React from 'react';
 import SignUpButton from './SignUpButton';
+import Burger from './Burger';
 import { Link } from 'react-router-dom'; 
 
 const NavBar = () => {
     return(
-      <div className="NavBar navbar navbar-light bg-light static-top">
-        <div className="nav-links container">
-          <Link className="logo" to="/"><i class="fas fa-running"></i> </Link>
-          <Link className="link" to="/">Home</Link>
-          <Link className="link" to="/about">About</Link>
-          <Link className="link" to='/contact'>Contact</Link>
-          <Link className="link" to="/feeds">Feeds</Link>
-          <SignUpButton>Sign Up</SignUpButton>
-        </div>
+      <div className="NavBar navbar navbar-light  bg-light static-top">
+        <Link className="logo" to="/"><i class="fas fa-running"></i> </Link>
+        <Burger />
+        <SignUpButton>Sign Up</SignUpButton>  
       </div>
+    
     )
   }
 
 export default NavBar;
 
+
+/*<nav class="navbar navbar-expand-md bg-dark navbar-dark">
+  <a class="navbar-brand" href="#">Navbar</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="collapsibleNavbar">
+    <ul class="navbar-nav">
+      <li class="nav-item">
+        <a class="nav-link" href="#">Link</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Link</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Link</a>
+      </li>    
+    </ul>
+  </div>  
+</nav>*/
